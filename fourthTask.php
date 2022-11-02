62648b4c7442d1f4ff3700cd5c198554b00a2eac
kadmin <kadmin@gmail.com>
2022-11-02 12:40:11 +0700

    update

---
+++ fourthTask.php
@@ -0,0 +1,11 @@
+<?php
+
+
+function getMinNumber(float $firstNum, float $secondNumber, float $thirdNumber){
+    return min($firstNum, $secondNumber, $thirdNumber);
+}
+
+echo getMinNumber(49.01, 29.0312, -30.03);
+
+
+
